fc4921d70e3f7b137c649be507704a481c46bffb
StellaKim1230 <stellakim1230@gmail.com>
2022-07-27 12:15:58 +0900

    Fix markup.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState } from 'react'
 import { useList } from '../src/hooks/useList'
 import { dummyDatas } from '../src/dummy'
 
@@ -9,21 +9,38 @@ interface List {
 
 function App() {
   const { state, append, popleft, pop, remove } = useList<List>(dummyDatas)
-  console.log('state', state)
+  const [data, setData] = useState<string>('')
 
   return (
-    <div>
-      <button onClick={() => append({ id: 'test', title: 'title' })}>
-        append
+    <div style={{ padding: 40 }}>
+      <input value={data} onChange={(e) => setData(e.target.value)} />
+      <br />
+      <br />
+      <button
+        onClick={() => {
+          append({ id: new Date().toString(), title: data })
+          setData('')
+        }}
+        style={{ marginRight: 16 }}
+      >
+        추가
       </button>
-      <button onClick={() => popleft()}>popleft</button>
-      <button onClick={() => pop()}>pop</button>
-      <button onClick={() => remove(2)}>remove</button>
+      <button onClick={popleft} style={{ marginRight: 16 }}>
+        맨 처음 요소 삭제
+      </button>
+      <button onClick={pop} style={{ marginRight: 16 }}>
+        맨 뒤에 요소 삭제
+      </button>
 
       <ul>
         {state &&
           state.length > 0 &&
-          state.map((item, index) => <li key={index}>{item.title}</li>)}
+          state.map((item, index) => (
+            <div style={{ marginBottom: 16 }}>
+              <li key={index}>{item.title}</li>
+              <button onClick={() => remove(index)}>삭제</button>
+            </div>
+          ))}
       </ul>
     </div>
   )
